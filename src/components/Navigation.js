6f584449469425d6f64530dd6587d7ec96003b5c
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-16 19:08:52 -0600

    Finish up look of the Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,17 +4,26 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import logo from "./logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#ffffff",
   },
+  logo: {
+    width: "75px",
+    marginTop: "10px",
+  },
   title: {
     flexGrow: 1,
   },
   button: {
-    color: "#000000",
+    color: "#6b7280",
     marginRight: "20px",
+    "&:hover": {
+      backgroundColor: "#EDF5EA",
+      color: "#374151",
+    },
   },
 }));
 
@@ -26,8 +35,9 @@ const Navigation = () => {
       <AppBar className={classes.root} position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            News
+            <img src={logo} className={classes.logo} alt="Logo" />
           </Typography>
+
           <Button className={classes.button}>My Plants</Button>
           <Button className={classes.button}>Add Plant</Button>
           <Button className={classes.button}>My Account</Button>
