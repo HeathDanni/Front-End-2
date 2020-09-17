61ca8bfd0fde4d521a4a5cb4abd9438e65e4a9ea
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-17 06:30:32 -0600

    removed import from myplants

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Navigation from "./components/Navigation";
+import MyPlants from "./components/MyPlants";
 
 function App() {
   return (
--- src/components/MyPlants.js
+++ src/components/MyPlants.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "../App.css";
 import {Card} from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 
