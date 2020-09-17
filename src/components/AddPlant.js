61c3cb74e7fae9e197664f0a0254d7a5d3767539
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-17 08:34:36 -0600

    created addplant component

---
+++ src/components/AddPlant.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Card, TextField, MenuItem, InputLabel, Select} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        display:'flex',
+        flexDirection: 'column',
+        marginLeft: "20%",
+    },
+    card: {
+        width: "75%",
+        height: "250px"
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: "10px"
+    },
+    input: {
+        marginBottom: '20px',
+    }
+});
+
+const AddPlant = () => {
+      
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <h2>Add a plant!</h2>
+            <Card 
+            variant="outlined"
+            className={classes.card}
+            >
+                <form className={classes.form}>
+                    <TextField id="outlined-basic" label="Plant's Type" variant="outlined" className={classes.form}/>
+                    <TextField id="outlined-basic" label="Plant's Nickname" variant="outlined" className={classes.form}/>
+                    <InputLabel id="label" className={classes.form} >Watering Frequency</InputLabel>
+                    <Select labelId="label" id="select" value="Every 3 days">
+                        <MenuItem value="Everyday">Everyday</MenuItem>
+                        <MenuItem value="3">Every 3 days (recommended for most house plants)</MenuItem>
+                        <MenuItem value="7">Once a week</MenuItem>
+                        <MenuItem value="30">Once a month (recommended for cacti)</MenuItem>
+                    </Select>
+                </form>
+            </Card>
+        </div>
+    )
+};
+
+export default AddPlant;
