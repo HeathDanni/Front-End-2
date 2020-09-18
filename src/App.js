02fa7bbae39992009c754190ebca2c24b4015244
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-17 18:26:41 -0600

    Set up userContext and Context Provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { UserContext } from "./contexts/userContext";
 import Login from "./components/Login";
 import Navigation from "./components/Navigation";
 import MyPlants from "./components/MyPlants";
 import AddPlant from "./components/AddPlant";
 
 function App() {
+  const [user, setUser] = useState({});
+
   return (
-    <Router>
-      <Navigation />
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/myplants">
-          <MyPlants />
-        </Route>
-        <Route path="/addplant">
-          <AddPlant />
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/myplants">
+            <MyPlants />
+          </Route>
+          <Route path="/addplant">
+            <AddPlant />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
