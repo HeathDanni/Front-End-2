f92acce03445befb80307035f381c3d5249b9fdd
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-18 19:45:47 -0600

    set up tab switcher for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import { Card, Button, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     display: "flex",
+    flexDirection: "column",
+  },
+
+  pageName: {
+    marginLeft: "25%",
+    color: "#6b7280",
+  },
+
+  cards: {
+    width: "50%",
+    display: "flex",
+    marginLeft: "25%",
+    flexDirection: "column",
+    marginBottom: "20px",
+  },
+  textField: {
+    marginBottom: "10px",
+  },
+
+  cardText: {
+    margin: "20px",
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  buttonDiv: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "100%",
+  },
+
+  button: {
+    marginTop: "5%",
+    marginBottom: "20px",
+    fontSize: "12px",
+    backgroundColor: "#669966",
+    "&:hover": {
+      backgroundColor: "#6b7280",
+    },
   },
 });
 
@@ -32,21 +70,34 @@ const Login = () => {
 
   return (
     <div>
-      <h1>Login Component</h1>
-      <form onSubmit={login} className={classes.root}>
-        <label>Username:</label>
-        <TextField
-          className={classes.textField}
-          id="outlined-basic"
-          label="Username"
-          variant="outlined"
-        />
-        <label>Password:</label>
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
-        <Button variant="contained" color="primary">
-          Log In
-        </Button>
-      </form>
+      <h1 className={classes.pageName}>Log In</h1>
+      <div className={classes.root}>
+        <Card className={classes.cards} variant="outlined">
+          <div className={classes.cardText}>
+            <form onSubmit={login} className={classes.root}>
+              <TextField
+                className={classes.textField}
+                id="outlined-basic"
+                label="Username"
+                variant="outlined"
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Log In
+              </Button>
+            </form>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,35 +1,96 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import { Card, Button, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    root: {
-      display: "flex",
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  pageName: {
+    marginLeft: "25%",
+    color: "#6b7280",
+  },
+
+  cards: {
+    width: "50%",
+    display: "flex",
+    marginLeft: "25%",
+    flexDirection: "column",
+    marginBottom: "20px",
+  },
+  textField: {
+    marginBottom: "10px",
+  },
+
+  cardText: {
+    margin: "20px",
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  buttonDiv: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "100%",
+  },
+
+  button: {
+    marginTop: "5%",
+    marginBottom: "10px",
+    fontSize: "12px",
+    backgroundColor: "#669966",
+    "&:hover": {
+      backgroundColor: "#6b7280",
     },
-  });
+  },
+});
 
 const RegisterForm = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <h1>Register User Form</h1>
-            <form className={classes.root}>
-                <label>Username:</label>
-                <TextField
-                    className={classes.root}
-                    id= 'outlined-basic'
-                    label='Username'
-                    variant='outlined'
-                />
-                <label>Password:</label>
-                <TextField id='outlined-basic' label='password' variant='outlined'/>
-                <Button variant='contained' color='primary'>Create User</Button>
+  const handleSubmit = () => {
+    console.log("submitting");
+  };
+  return (
+    <div>
+      <h1 className={classes.pageName}>Register</h1>
+      <div className={classes.root}>
+        <Card className={classes.cards} variant="outlined">
+          <div className={classes.cardText}>
+            <form onSubmit={handleSubmit} className={classes.root}>
+              <TextField
+                className={classes.textField}
+                id="outlined-basic"
+                label="Username"
+                variant="outlined"
+              />
+              <TextField
+                className={classes.textField}
+                id="outlined-basic"
+                label="Phone Number"
+                variant="outlined"
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Create My Account
+              </Button>
             </form>
-        </div>
-    );
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterForm;
