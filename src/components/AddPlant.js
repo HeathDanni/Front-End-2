4a842db97282a5a6e014c5a559a05521085aa90d
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-20 20:51:38 -0600

    form can send and receive data from server on submit

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card, TextField, MenuItem, InputLabel, Select, Button} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+import axios from 'axios';
 
 const useStyles = makeStyles({
     root: {
@@ -46,29 +48,101 @@ const AddPlant = () => {
       
     const classes = useStyles();
 
+    const [formData, setFormData] = useState({
+        type: "",
+        name: "",
+        frequency: ""
+    });
+
+    // const [errors, setErrors] = useState({
+    //     type: "",
+    //     name: "",
+    //     frequency: ""
+    // });
+
+    const changeHandler = (e) => {
+        e.persist()
+        
+        setFormData({
+            ...formData, [e.target.name]: e.target.value
+        });
+    };
+
+    console.log(formData);
+
+    const [plant, setPlant] = useState([]);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        axios
+            .post('https://reqres.in/api/users', formData)
+            .then(res => {console.log('res', res)
+                setPlant(
+                    ...plant,
+                        [res.data]
+                )})
+            .catch(err => {console.log('err', err)})
+
+        setFormData({
+            type: "",
+            name: "",
+            frequency: ""
+        })
+    };
+
+    console.log('plant', plant)
+
     return (
+
         <div className={classes.root}>
             <h1 className={classes.pageName}>Add a plant!</h1>
             <Card 
-            variant="outlined"
-            className={classes.card}
+                variant="outlined"
+                className={classes.card}
             >
-                <form className={classes.form}>
-                    <TextField id="outlined-basic" label="Plant Type" variant="outlined" className={classes.form}/>
-                    <TextField id="outlined-basic" label="Plant Nickname" variant="outlined" className={classes.form}/>
+                <form className={classes.form} onSubmit={submitForm}>
+
+                    <TextField 
+                        id="outlined-basic"
+                        name="type"
+                        label="Plant Type" 
+                        variant="outlined" 
+                        className={classes.form} 
+                        onChange={changeHandler}
+                        value={formData.type}/>
+
+                    <TextField 
+                        id="outlined-basic" 
+                        name="name"
+                        label="Plant Nickname" 
+                        variant="outlined" 
+                        className={classes.form}
+                        onChange={changeHandler}
+                        value={formData.name}/>
+
                     <InputLabel id="label" className={classes.form} >Watering Frequency</InputLabel>
-                    <Select labelId="label" id="select" value="Every 3 days">
-                        <MenuItem value="Everyday">Everyday</MenuItem>
-                        <MenuItem value="3">Every 3 days (recommended for most house plants)</MenuItem>
-                        <MenuItem value="7">Once a week</MenuItem>
-                        <MenuItem value="30">Once a month (recommended for cacti)</MenuItem>
+
+                    <Select 
+                        labelId="label" 
+                        id="select"
+                        name="frequency"
+                        value={formData.frequency}
+                        onChange={changeHandler}>
+                            <MenuItem value="Everday">Everyday</MenuItem>
+                            <MenuItem value="Every 3 days">Every 3 days (recommended for most house plants)</MenuItem>
+                            <MenuItem value="Once a week">Once a week</MenuItem>
+                            <MenuItem value="Once a month">Once a month (recommended for cacti)</MenuItem>
                     </Select>
+
                     <Button 
-                    variant="contained" 
-                    color="primary"
-                    className={classes.button}>
-                    Add Plant
+                        type='submit'
+                        variant="contained" 
+                        color="primary"
+                        className={classes.button}>
+                        Add Plant
                     </Button>
+
                 </form>
             </Card>
         </div>
