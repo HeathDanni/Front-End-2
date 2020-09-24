1feac6601ded0c4fc6ac7cf1cbda536d1874a080
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-24 17:47:10 -0600

    remove log from axiosWithAuth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,11 +3,10 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   console.log(`This is the token: ${token}`);
-  const myRequest = axios.create({
+
+  return axios.create({
     headers: {
       Authorization: token,
     },
   });
-  console.log(`This is the axios request: ${myRequest}`);
-  return myRequest;
 };
