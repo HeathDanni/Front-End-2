31b7f54ef2af0d6e4ba8176ee8b2aabe6639aa30
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-24 17:38:48 -0600

    add more logging for the axios request

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,9 +3,11 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   console.log(`This is the token: ${token}`);
-  return axios.create({
+  const myRequest = axios.create({
     headers: {
       Authorization: token,
     },
   });
+  console.log(`This is the axios request: ${myRequest}`);
+  return myRequest;
 };
